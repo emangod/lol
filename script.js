66e952c8068f5b8b3d3bd5cf727aaee7710ca6c9
emangod <78166165+emangod@users.noreply.github.com>
2021-02-25 10:47:55 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function one()
+{
+  window.open("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDFTStw6X5PuwqbUDgRVsDV2pf09qN62Afe4cYfHfclUctZu2EHw:https://cdn.cnn.com/cnnnext/dam/assets/160927210830-tk-ah0927-exlarge-169.jpg", null, "width=999,height=999");
+two();
+three();
+}
+function two()
+{
+ window.open("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDFTStw6X5PuwqbUDgRVsDV2pf09qN62Afe4cYfHfclUctZu2EHw:https://cdn.cnn.com/cnnnext/dam/assets/160927210830-tk-ah0927-exlarge-169.jpg");
+ one();
+}
+function three()
+{
+  window.open("https://static.tvtropes.org/pmwiki/pub/images/bridget.png")
+one()
+two()
+}
+function four()
+{
+  window.open("https://static.tvtropes.org/pmwiki/pub/images/bridget.png")
+one()
+two()
+}
+function five()
+{
+  window.open("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDFTStw6X5PuwqbUDgRVsDV2pf09qN62Afe4cYfHfclUctZu2EHw:https://cdn.cnn.com/cnnnext/dam/assets/160927210830-tk-ah0927-exlarge-169.jpg", null, "width=999,height=999");
+two();
+three();
+}
