0934905ebc817138136c91021030d024df5e98b5
emangod <78166165+emangod@users.noreply.github.com>
2021-02-26 10:16:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,12 @@ function two()
 {
  window.open("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDFTStw6X5PuwqbUDgRVsDV2pf09qN62Afe4cYfHfclUctZu2EHw:https://cdn.cnn.com/cnnnext/dam/assets/160927210830-tk-ah0927-exlarge-169.jpg");
  one();
+}
 function three()
 {
-  window.open("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDFTStw6X5PuwqbUDgRVsDV2pf09qN62Afe4cYfHfclUctZu2EHw:https://cdn.cnn.com/cnnnext/dam/assets/160927210830-tk-ah0927-exlarge-169.jpg", null, "width=999,height=999");
-two();
-three();
+  window.open("https://static.tvtropes.org/pmwiki/pub/images/bridget.png")
+one()
+two()
 }
 function four()
 {
@@ -21,18 +22,6 @@ one()
 two()
 }
 function five()
-{
-  window.open("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDFTStw6X5PuwqbUDgRVsDV2pf09qN62Afe4cYfHfclUctZu2EHw:https://cdn.cnn.com/cnnnext/dam/assets/160927210830-tk-ah0927-exlarge-169.jpg", null, "width=999,height=999");
-two();
-three();
-}
-function six()
-{
-  window.open("https://www.pacdv.com/sounds/fart-sounds/fart-1.wav", null, "width=999,height=999");
-two();
-three();
-}
-function seven()
 {
   window.open("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDFTStw6X5PuwqbUDgRVsDV2pf09qN62Afe4cYfHfclUctZu2EHw:https://cdn.cnn.com/cnnnext/dam/assets/160927210830-tk-ah0927-exlarge-169.jpg", null, "width=999,height=999");
 two();
