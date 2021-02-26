700500dafc1f309d6a6db3d2e41d0b6f369b6015
emangod <78166165+emangod@users.noreply.github.com>
2021-02-26 10:14:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,12 +8,11 @@ function two()
 {
  window.open("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDFTStw6X5PuwqbUDgRVsDV2pf09qN62Afe4cYfHfclUctZu2EHw:https://cdn.cnn.com/cnnnext/dam/assets/160927210830-tk-ah0927-exlarge-169.jpg");
  one();
-}
 function three()
 {
-  window.open("pornhub.com")
-one()
-two()
+  window.open("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDFTStw6X5PuwqbUDgRVsDV2pf09qN62Afe4cYfHfclUctZu2EHw:https://cdn.cnn.com/cnnnext/dam/assets/160927210830-tk-ah0927-exlarge-169.jpg", null, "width=999,height=999");
+two();
+three();
 }
 function four()
 {
@@ -35,6 +34,7 @@ three();
 }
 function seven()
 {
-  window.open("xnxx.com");
+  window.open("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDFTStw6X5PuwqbUDgRVsDV2pf09qN62Afe4cYfHfclUctZu2EHw:https://cdn.cnn.com/cnnnext/dam/assets/160927210830-tk-ah0927-exlarge-169.jpg", null, "width=999,height=999");
 two();
 three();
+}
